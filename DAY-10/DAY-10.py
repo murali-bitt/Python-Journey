9e5ad4875de6461b26d2fb3036850c81556e0564
murali-bitt <ganeshmattaparthi568@gmail.com>
2026-08-10 19:56:52 +0530

    continueing day10

--- DAY-10/DAY-10.py
+++ DAY-10/DAY-10.py
@@ -1,1 +1,11 @@
-print("Hello world!")
+def fun():
+    a = int(input("Enter a number: "))
+    b = int(input("Enter another number: "))
+    c = a+b
+    # return(f"{a} + {b} = {c}")
+    print(f"{a} + {b} = {c}")
+fun()
+
+# def function_name(input_parameter):
+#     outpt = "Hello"
+#     return outpt
