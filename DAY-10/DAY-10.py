a224d7e609d0f594f3b584847a4dc90122fe8073
murali-bitt <ganeshmattaparthi568@gmail.com>
2026-08-11 23:28:45 +0530

    added few lines

--- DAY-10/DAY-10.py
+++ DAY-10/DAY-10.py
@@ -1,11 +1,12 @@
-def fun():
-    a = int(input("Enter a number: "))
-    b = int(input("Enter another number: "))
-    c = a+b
-    # return(f"{a} + {b} = {c}")
-    print(f"{a} + {b} = {c}")
-fun()
-
-# def function_name(input_parameter):
-#     outpt = "Hello"
-#     return outpt
+# def fun():
+#     a = int(input("Enter a number: "))
+#     b = int(input("Enter another number: "))
+#     c = a+b
+#     # return(f"{a} + {b} = {c}")
+#     print(f"{a} + {b} = {c}")
+# fun()
+n = input("Enter your name: ")
+b = input("Enter the total amount: $")
+def receipt(name,bill):
+    print(f"Hello {name}, \nThanks for shopping.\nthe bill made was: ${bill}")
+receipt(n,b)
