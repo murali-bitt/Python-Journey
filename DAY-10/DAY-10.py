dbd7bc878b7262168a9e4b6a44e868ec178b39fc
murali-bitt <ganeshmattaparthi568@gmail.com>
2026-08-12 22:42:47 +0530

    made function to find whether the entered year is a leap year or not

--- DAY-10/DAY-10.py
+++ DAY-10/DAY-10.py
@@ -5,8 +5,21 @@
 #     # return(f"{a} + {b} = {c}")
 #     print(f"{a} + {b} = {c}")
 # fun()
-n = input("Enter your name: ")
-b = input("Enter the total amount: $")
-def receipt(name,bill):
-    print(f"Hello {name}, \nThanks for shopping.\nthe bill made was: ${bill}")
-receipt(n,b)
+# n = input("Enter your name: ")
+# b = input("Enter the total amount: $")
+# def receipt(name,bill):
+#     print(f"Hello {name}, \nThanks for shopping.\nthe bill made was: ${bill}")
+# receipt(n,b)
+
+# def is_leap_year(year):
+#     a= 0
+#     if year % 4 == 0:
+#         a += 1
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 a += 1
+#         else:
+#             a +=1
+#     return (a>=2)
+
+# is_leap_year(2100)
