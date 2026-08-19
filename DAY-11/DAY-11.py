1fa68644ba5bb2af854b971dc946309e2f34c1eb
murali-bitt <ganeshmattaparthi568@gmail.com>
2026-08-19 23:42:51 +0530

    edit

--- DAY-11/DAY-11.py
+++ DAY-11/DAY-11.py
@@ -1,3 +1,7 @@
-a = int(input("Enter a number: "))
-b = int(input("Enter a number: "))
-print(a+b)
+# a = int(input("Enter a number: "))
+# b = int(input("Enter a number: "))
+# print(a+b)
+a = 1
+while a<=10:
+    print(a)
+    a += 1
